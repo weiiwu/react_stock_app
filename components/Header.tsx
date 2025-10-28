d2767df393dc43fe54d3dfb3bb8c5fb262b92acb
weiiwu <master_wu51@hotmail.com>
2025-10-28 15:03:38 +1000

    Search added

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,12 @@ import Image from "next/image"
 import Link from "next/link"
 import NavItems from "./NavItems"
 import UserDropdown from "./UserDropdown"
+import { searchStocks } from "@/lib/actions/finnhub.actions"
 
-const Header = ({ user }: { user: User }) => {
+const Header = async ({ user }: { user: User }) => {
+  const initialStocks = await searchStocks();
+
+
   return (
     <header className="sticky top-0 header">
       <div className="container header-wrapper">
@@ -11,9 +15,9 @@ const Header = ({ user }: { user: User }) => {
           <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
         </Link>
         <nav className="hidden sm:block">
-          <NavItems />
+          <NavItems initialStocks={initialStocks} />
         </nav>
-        <UserDropdown user={user} />
+        <UserDropdown user={user} initialStocks={initialStocks} />
       </div>
 
     </header>
